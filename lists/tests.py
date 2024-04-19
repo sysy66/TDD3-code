284df8dbea157a649935090d95391e21faccdf90
caolv <shaoyinshaoyang@gmail.com>
2024-04-19 16:29:59 +0800

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -7,13 +7,6 @@ class HomePageTest(TestCase):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
     
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-        response = self.client.get("/")
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
-
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
         self.assertEqual(Item.objects.count(), 1)
@@ -22,7 +15,7 @@ class HomePageTest(TestCase):
     
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/")
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
     
     def test_only_saves_items_when_necessary(self):
         self.client.get("/")
@@ -34,15 +27,28 @@ class ItemModelTest(TestCase):
         first_item = Item()
         first_item.text = "The first (ever) list item"
         first_item.save()
-
+        
         second_item = Item()
         second_item.text = "Item the second"
         second_item.save()
-
+        
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
-
+        
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
         self.assertEqual(second_saved_item.text, "Item the second")
+
+
+class ListViewTest(TestCase):
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+    
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
