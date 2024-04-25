269555a6fbe31d8d77f41ff895ac5d3019dad2d8
caolv <shaoyinshaoyang@gmail.com>
2024-04-25 16:27:57 +0800

    Hack FT runner to be able to test docker

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 5
 
@@ -11,6 +12,9 @@ MAX_WAIT = 5
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
+        test_server = os.environ.get("TEST_SERVER")
+        if test_server:
+            self.live_server_url = "http://" + test_server
     
     def tearDown(self):
         self.browser.quit()
