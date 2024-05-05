f9c27133d7fcdf3acb9854406b64e1c5d10fcfb3
caolv <shaoyinshaoyang@gmail.com>
2024-05-05 16:33:47 +0800

    Refactor list_view to handle new item POSTs

--- src/lists/views.py
+++ src/lists/views.py
@@ -9,6 +9,9 @@ def home_page(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"], list=our_list)
+        return redirect(f"/lists/{our_list.id}/")
     return render(request, "list.html", {"list": our_list})
 
 
@@ -23,9 +26,3 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, "home.html", {"error": error})
     return redirect(f"/lists/{nulist.id}/")
-
-
-def add_item(request, list_id):
-    our_list = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_list)
-    return redirect(f"/lists/{our_list.id}/")
