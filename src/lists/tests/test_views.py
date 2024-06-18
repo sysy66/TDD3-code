b364c2ecc784b12d70bc7277b06b1c0a4c3f61c3
caolv <shaoyinshaoyang@gmail.com>
2024-06-18 18:02:46 +0800

    A More Complex Form to Handle Uniqueness Validation

--- src/lists/tests/test_views.py
+++ src/lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.test import TestCase
 from lists.models import Item, List
 from django.utils.html import escape
@@ -93,6 +95,20 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/lists/{mylist.id}/")
         self.assertIsInstance(response.context["form"], ItemForm)
         self.assertContains(response, 'name="text"')
+    
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+        
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
