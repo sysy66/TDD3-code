e30f7a8610b8a0d0a23a47cd5d2c04c9948a0750
caolv <shaoyinshaoyang@gmail.com>
2024-08-10 04:37:05 +0800

    De-spiking. For now, the models layer is done.

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "lists",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -108,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
