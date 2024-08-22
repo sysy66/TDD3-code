3a4de529022fe9e0604cd37b004a97e61f207746
caolv <shaoyinshaoyang@gmail.com>
2024-08-22 22:10:06 +0800

    helper function for retrieving emails via POP3. And use it in FT test_login.

--- src/superlists/settings.py
+++ src/superlists/settings.py
@@ -152,6 +152,6 @@ LOGGING = {
 # Email setting
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
