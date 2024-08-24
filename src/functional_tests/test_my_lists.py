bf257726a18eeaeb3db2efb6bf0b141b32321c39
caolv <shaoyinshaoyang@gmail.com>
2024-08-24 17:06:53 +0800

    Helper function to add list items and use it in fts. Start on first real version of my lists FT.

--- src/functional_tests/test_my_lists.py
+++ src/functional_tests/test_my_lists.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.by import By
+
 from django.conf import settings
 from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
 from django.contrib.sessions.backends.db import SessionStore
@@ -14,8 +16,9 @@ class MyListsTest(FunctionalTest):
         session[SESSION_KEY] = user.pk
         session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
         session.save()
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
+        
+        # to set a cookie we need to first visit the domain.
+        # 404 pages load the quickest!
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(
             dict(
@@ -24,17 +27,52 @@ class MyListsTest(FunctionalTest):
                 path="/",
             )
         )
-        
+    
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         if self.test_server:
             # TODO: 21.5. Managing the Test Database on Staging
             return
-        email = "edith@example.com"
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_out(email)
-
-        # Edith is a logged-in user
-        self.create_pre_authenticated_session(email)
-        self.browser.get(self.live_server_url)
-        self.wait_to_be_logged_in(email)
         
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session("edith@example.com")
+        
+        # She goes to the home page and starts a list
+        self.browser.get(self.live_server_url)
+        self.add_list_item("Reticulate splines")
+        self.add_list_item("Immanentize eschaton")
+        first_list_url = self.browser.current_url
+        
+        # She notices a "My lists" link, for the first time.
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+        
+        # She sees that her list is in there, named according to its
+        # first list item
+        self.wait_for(
+            lambda: self.browser.find_element(By.LINK_TEXT, "Reticulate splines")
+        )
+        self.browser.find_element(By.LINK_TEXT, "Reticulate splines").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
+        
+        # She decides to start another list, just to see
+        self.browser.get(self.live_server_url)
+        self.add_list_item("Click cows")
+        second_list_url = self.browser.current_url
+        
+        # Under "my lists", her new list appears
+        self.browser.find_element(By.LINK_TEXT, "My lists").click()
+        self.wait_for(lambda: self.browser.find_element(By.LINK_TEXT, "Click cows"))
+        self.browser.find_element(By.LINK_TEXT, "Click cows").click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+        
+        # She logs out.  The "My lists" option disappears
+        self.browser.find_element(By.LINK_TEXT, "Log out").click()
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_elements(By.LINK_TEXT, "My lists"),
+                [],
+            )
+        )
